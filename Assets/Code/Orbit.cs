08b6a03dfee60fc0ca2367ba81f97bbef13ed1a4
Vladimir Mitrovic <v@brutalism.rs>
2015-11-05 23:40:56 +0100

    Initial commit, looking at a cube with rotation input from the gyroscope.

---
+++ Assets/Code/Orbit.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Orbit : MonoBehaviour
+{
+	#region Editor public fields
+	#endregion
+
+	#region Public properties
+	#endregion
+
+	#region Private fields
+
+	private Quaternion rotationOffset;
+	private float distanceFromOrigin;
+
+	#endregion
+
+	#region Unity methods
+
+	void Start()
+	{
+		rotationOffset = Quaternion.Euler(90f, 0f, 0f);
+		distanceFromOrigin = transform.position.magnitude;
+	}
+
+	void Update()
+	{
+		var gyroRotation = rotationOffset * ConvertRotation(Input.gyro.attitude);
+
+		transform.position = Vector3.zero;
+		transform.rotation = gyroRotation;
+		transform.position = -transform.forward * distanceFromOrigin;
+	}
+
+	#endregion
+
+	#region Private methods
+
+	private static Quaternion ConvertRotation(Quaternion q)
+	{
+		return new Quaternion(q.x, q.y, -q.z, -q.w);
+	}
+
+	#endregion
+}
