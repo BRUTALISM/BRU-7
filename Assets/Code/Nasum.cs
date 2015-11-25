0308fd0f0fc7030505d868245299ede1bff847f2
Vladimir Mitrovic <v@brutalism.rs>
2015-11-25 18:14:45 +0100

    Added Gaussian random number generation to Nasum.

--- Assets/Code/Nasum.cs
+++ Assets/Code/Nasum.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 
+/// <summary>
+/// A random number generator class which uses a single internal seed to generate values. Unlike Unity's Random class, this class'
+/// generated values are guaranteed to be the same across invokations if both the seeds and the method invokation orders are the same.
+/// </summary>
 public static class Nasum
 {
 	private static Random random = null;
 
 	private static int seed;
+
+	/// <summary>
+	/// The seed used by all methods of this class.
+	/// </summary>
 	public static int Seed
 	{
 		get { return seed; }
@@ -17,6 +25,9 @@ public static class Nasum
 		}
 	}
 
+	/// <summary>
+	/// A random value in the [0, 1] range.
+	/// </summary>
 	public static float Value
 	{
 		get
@@ -26,6 +37,9 @@ public static class Nasum
 		}
 	}
 
+	/// <summary>
+	/// A random rotation.
+	/// </summary>
 	public static UnityEngine.Quaternion Rotation
 	{
 		get
@@ -39,13 +53,56 @@ public static class Nasum
 		random = new Random(seed);
 	}
 
+	/// <summary>
+	/// Returns a random float in the [min, max] range.
+	/// </summary>
 	public static float Range(float min, float max)
 	{
 		return min + Value * (max - min);
 	}
 
+	/// <summary>
+	/// Returns a random integer in the [min, max - 1] range.
+	/// </summary>
 	public static int Range(int min, int max)
 	{
 		return min + (int)(Value * (max - min - 1));
+	}
+
+	/// <summary>
+	/// Returns a random number generated using the standard distribution with the given <paramref name="mean"/> and
+	/// <paramref name="standardDeviation"/>.
+	/// </summary>
+	public static float Gaussian(float mean, float standardDeviation)
+	{
+		// Code taken from: http://stackoverflow.com/questions/5817490/implementing-box-mueller-random-number-generator-in-c-sharp
+		float u, v, S;
+
+		do
+		{
+			u = 2f * Value - 1f;
+			v = 2f * Value - 1f;
+			S = u * u + v * v;
+		}
+		while (S >= 1f);
+
+		var fac = UnityEngine.Mathf.Sqrt(-2f * UnityEngine.Mathf.Log(S) / S);
+
+		var normalRandom = u * fac;
+
+		return normalRandom * standardDeviation + mean;
+	}
+
+	/// <summary>
+	/// Returns a random number generated using the standard distribution with the given <paramref name="mean"/> and
+	/// <paramref name="standardDeviation"/>, in the range given by <paramref name="rangeMin"/> and <paramref name="rangeMax"/>. The
+	/// algorithm first obtains a gaussian random variable in a standard way, and then checks if it's within the given range. If it's not,
+	/// the random roll is performed again using a uniform distribution in the [rangeMin, rangeMax] range.
+	/// </summary>
+	public static float GaussianInRange(float mean, float standardDeviation, float rangeMin, float rangeMax)
+	{
+		var randomValue = Gaussian(mean, standardDeviation);
+		if (randomValue < rangeMin || randomValue > rangeMax) return Range(rangeMin, rangeMax);
+		return randomValue;
 	}
 }
---
+++ Assets/Code/Test/NasumTest.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+[ExecuteInEditMode]
+public class NasumTest : MonoBehaviour
+{
+	#region Editor public fields
+
+	public int SamplesPerTest = 100;
+	public float YScale = 1f;
+
+	#endregion
+
+	#region Public properties
+	#endregion
+
+	#region Private fields
+
+	private List<Dictionary<int, int>> testResults = new List<Dictionary<int, int>>();
+	private List<Color> gizmoColors = new List<Color>();
+
+	#endregion
+
+	#region Unity methods
+
+	void OnEnable()
+	{
+		// Generate some test data
+		GenerateTest(() => Nasum.Gaussian(0f, 1f));
+		GenerateTest(() => Nasum.Gaussian(2f, 1f));
+		GenerateTest(() => Nasum.Gaussian(0f, 5f));
+		GenerateTest(() => Nasum.GaussianInRange(0f, 5f, -5f, 5f));
+		GenerateTest(() => Nasum.GaussianInRange(0f, 2f, -5f, 5f));
+		GenerateTest(() => Nasum.GaussianInRange(0f, 1f, -5f, 5f));
+	}
+
+	void OnDisable()
+	{
+		testResults.Clear();
+		gizmoColors.Clear();
+	}
+
+	void OnDrawGizmos()
+	{
+		for (int testNumber = 0; testNumber < testResults.Count; testNumber++)
+		{
+			Vector3 origin = transform.position + Vector3.forward * testNumber * 10f;
+			var testResult = testResults[testNumber];
+
+			Gizmos.color = gizmoColors[testNumber];
+			foreach (var keyValuePair in testResult)
+			{
+				var index = keyValuePair.Key;
+				var occurences = keyValuePair.Value;
+				Gizmos.DrawWireCube(origin + new Vector3(index, (YScale * occurences) / 2, 0f),
+					new Vector3(0.1f, YScale * occurences, 0.1f));
+			}
+		}
+	}
+
+	#endregion
+
+	#region Test generation
+
+	private void GenerateTest(System.Func<float> randomGenerator)
+	{
+		var resultsDictionary = new Dictionary<int, int>();
+		for (int i = 0; i < SamplesPerTest; i++)
+		{
+			var randomValue = randomGenerator();
+			var flooredRandom = Mathf.FloorToInt(randomValue);
+			if (resultsDictionary.ContainsKey(flooredRandom)) resultsDictionary[flooredRandom]++;
+			else resultsDictionary[flooredRandom] = 1;
+		}
+
+		testResults.Add(resultsDictionary);
+
+		gizmoColors.Add(new HSLColor(Random.Range(0f, 1f), 1f, 0.5f));
+	}
+
+	#endregion
+}
